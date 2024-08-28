88664f9136895187f35958db218072e7ff0c7968
Fubbrr <cx330xing@163.com>
2024-08-28 16:06:11 +0800

    使用线程

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -1,3 +1,4 @@
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -5,7 +6,7 @@
 #include <pthread.h>
 #include <sys/time.h>
 
-#define NBUCKET 5
+#define NBUCKET 8
 #define NKEYS 100000
 
 struct entry {
@@ -16,7 +17,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
-
+pthread_mutex_t locks[NBUCKET];
 
 double
 now()
@@ -40,7 +41,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(&locks[i]);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -52,9 +53,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
-    insert(key, value, &table[i], table[i]);
-  }
-
+   insert(key, value, &table[i], table[i]);
+}
+   pthread_mutex_unlock(&locks[i]);
 }
 
 static struct entry*
@@ -104,6 +105,11 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
+
+
+  for(int i = 0; i < NBUCKET; i++){
+    pthread_mutex_init(&locks[i], NULL);
+  }  
 
 
   if (argc < 2) {
