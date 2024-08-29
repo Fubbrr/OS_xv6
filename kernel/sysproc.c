0b6b5877feb343774368f2863810288ef9832aff
Fubbrr <cx330xing@163.com>
2024-08-29 17:28:14 +0800

    pgtbl

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -80,9 +80,60 @@ sys_sleep(void)
 int
 sys_pgaccess(void)
 {
-  // lab pgtbl: your code here.
-  return 0;
+  uint64 base;  // 起始虚拟地址
+  uint64 mask;  // 地址掩码（用于存储结果）
+  int len;      // 需要检查的页数
+
+  pagetable_t pagetable = 0;  // 页表
+  unsigned int procmask = 0;  // 处理后的页访问标志
+  pte_t *pte;  // 页表条目指针
+
+  struct proc *p = myproc();  // 获取当前进程结构体
+
+  // 从系统调用参数中获取 base、len 和 mask 的值
+  if(argaddr(0, &base) < 0 || argint(1, &len) < 0 || argaddr(2, &mask) < 0)
+    return -1;  // 参数获取失败，返回错误代码 -1
+
+  // 限制 len 的值为 int 类型的位数
+  if (len > sizeof(int) * 8) 
+    len = sizeof(int) * 8;
+
+  // 遍历每一页
+  for(int i = 0; i < len; i++) {
+    pagetable = p->pagetable;  // 获取当前进程的页表
+      
+    // 检查 base 是否超出最大虚拟地址范围
+    if(base >= MAXVA)
+      panic("pgaccess");
+
+    // 遍历页表的不同层级
+    for(int level = 2; level > 0; level--) {
+      pte = &pagetable[PX(level, base)];  // 获取页表条目
+      if(*pte & PTE_V) {  // 如果页表条目有效
+        pagetable = (pagetable_t)PTE2PA(*pte);  // 更新页表为下一层的物理地址
+      } else {
+        return -1;  // 如果无效，返回错误代码 -1
+      }      
+    }
+    pte = &pagetable[PX(0, base)];  // 获取最底层页表条目
+    if(pte == 0)
+      return -1;  // 如果页表条目为空，返回错误代码 -1
+    if((*pte & PTE_V) == 0)
+      return -1;  // 如果页表条目无效，返回错误代码 -1
+    if((*pte & PTE_U) == 0)
+      return -1;  // 如果页表条目不允许用户访问，返回错误代码 -1
+    if(*pte & PTE_A) {  // 如果页表条目被访问
+      procmask = procmask | (1L << i);  // 设置对应位表示页被访问
+      *pte = *pte & (~PTE_A);  // 清除页表条目的访问位
+    }
+    base += PGSIZE;  // 移动到下一个页
+  }
+
+  pagetable = p->pagetable;  // 恢复原始页表
+  // 将 procmask 的结果复制到用户空间指定的地址 mask
+  return copyout(pagetable, mask, (char *)&procmask, sizeof(unsigned int));
 }
+
 #endif
 
 uint64
